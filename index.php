f5f8447a80cc0f400ad5402f5a71c6aa51214d92
arnoson <omasocke@hotmail.de>
2020-12-07 13:08:26 +0100

    Rename

--- index.php
+++ index.php
@@ -9,5 +9,5 @@ App::plugin('arnoson/kirby-stats', [
   'options' => [
     'sqlite' => kirby()->root() . '/site/plugins/kirby-stats/stats.sqlite'
   ],
-  'hooks' => require_once __DIR__ . '/hooks/index.php'
+  'hooks' => require_once __DIR__ . '/hooks/hooks.php'
 ]);
