d52a19c00d97f8e331ea52ec1c49b9ab2889678f
arnoson <omasocke@hotmail.de>
2022-12-14 14:14:14 +0100

    chore: load classes with `load()`

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-
 use Kirby\Cms\App;
+
+load([
+  'arnoson\\KirbyStats\\Analyzer' => __DIR__ . '/lib/Analyzer.php',
+  'arnoson\\KirbyStats\\Counters' => __DIR__ . '/lib/Counters.php',
+  'arnoson\\KirbyStats\\Helpers' => __DIR__ . '/lib/Helpers.php',
+  'arnoson\\KirbyStats\\Interval' => __DIR__ . '/lib/Interval.php',
+  'arnoson\\KirbyStats\\KirbyStats' => __DIR__ . '/lib/KirbyStats.php',
+]);
 
 App::plugin('arnoson/kirby-stats', [
   'options' => [
