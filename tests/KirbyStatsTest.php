3e1f250c04d744059430ac4fff135b0b46ad5769
arnoson <hallo@arno-schlipf.info>
2023-02-23 14:13:14 +0100

    refactor: switch to `pest`

--- tests/KirbyStatsTest.php
+++ tests/KirbyStatsTest.php
@@ -1,92 +1,102 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use arnoson\KirbyStats\Interval;
 use arnoson\KirbyStats\KirbyStats;
-use PHPUnit\Framework\TestCase;
 
-class KirbyStatsTest extends TestCase {
-  public function setUp(): void {
-    $_SERVER = [
-      'HTTP_USER_AGENT' =>
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
-      'HTTP_HOST' => 'localhost:8888',
-    ];
+beforeEach(function () {
+  $_SERVER = [
+    'HTTP_USER_AGENT' =>
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
+    'HTTP_HOST' => 'localhost:8888',
+  ];
+});
+
+it('provides data', function () {
+  $now = new DateTimeImmutable();
+  $from = $now->modify('yesterday');
+  $to = $now->modify('tomorrow');
+
+  $stats = new KirbyStats(Interval::HOUR);
+  $stats->handle('/test', $now);
+  $stats->handle('/test', $now);
+  $stats->handle('/test', $now);
+
+  $data = $stats->data(Interval::HOUR, $from, $to);
+  $time = Interval::startOf(Interval::HOUR, $now)->getTimestamp();
+  expect($data[$time]['paths']['/test']['counters']['visits'])->toBe(3);
+
+  $stats->remove();
+});
+
+it('handles data stored in a bigger interval than the current', function () {
+  $now = new DateTimeImmutable();
+  $from = $now->modify('yesterday');
+  $to = $now->modify('tomorrow');
+
+  $stats = new KirbyStats(Interval::HOUR);
+  $stats->handle('/test', $now);
+
+  $stats = new KirbyStats(Interval::DAY);
+  for ($i = 0; $i < 24; $i++) {
+    $stats->handle('/test', $now);
   }
 
-  public function testData() {
-    $now = new DateTimeImmutable();
-    $from = $now->modify('yesterday');
-    $to = $now->modify('tomorrow');
+  $data = $stats->data(Interval::HOUR, $from, $to);
+  $time = Interval::startOf(Interval::HOUR, $now)->getTimestamp();
+  // An hour with 1 visit, and a day with 24 visits. When retrieving the data
+  // hourly, the day should be split up in 24 hour intervals with one visit
+  // each. So all in all the hour should now have 2 visits.
+  $this->assertEquals($data[$time]['paths']['/test']['counters']['visits'], 2);
 
-    $stats = new KirbyStats();
-    $stats->handle('/test', $now);
-    $stats->handle('/test', $now);
-    $stats->handle('/test', $now);
+  $stats->remove();
+});
 
-    $data = $stats->data($from, $to, Interval::HOUR);
-    $time = Interval::startOf(Interval::HOUR, $now)->getTimestamp();
-    $this->assertEquals($data[$time]['paths']['/test']['visits'], 3);
+it('handles data stored in a smaller interval than the current', function () {
+  $now = new DateTimeImmutable();
+  $from = $now->modify('last week');
+  $to = $now->modify('next week');
 
-    $stats->remove();
-  }
+  $stats = new KirbyStats(Interval::WEEK);
+  $stats->handle('/test', $now);
 
-  public function testBiggerInterval() {
-    $now = new DateTimeImmutable();
-    $from = $now->modify('yesterday');
-    $to = $now->modify('tomorrow');
+  $stats = new KirbyStats(Interval::DAY);
+  $stats->handle('/test', $now);
 
-    $stats = new KirbyStats(Interval::HOUR);
-    $stats->handle('/test', $now);
+  $data = $stats->data(Interval::WEEK, $from, $to);
+  $time = Interval::startOf(Interval::WEEK, $now)->getTimestamp();
+  // A week and a day with 1 visit each. When retrieving the data weekly, the
+  // day should be added to the week.
+  expect($data[$time]['paths']['/test']['counters']['visits'])->toBe(2);
 
-    $stats = new KirbyStats(Interval::DAY);
-    for ($i = 0; $i < 24; $i++) {
-      $stats->handle('/test', $now);
-    }
+  $stats->remove();
+});
 
-    $data = $stats->data($from, $to, Interval::HOUR);
-    $time = Interval::startOf(Interval::HOUR, $now)->getTimestamp();
-    // An hour with 1 visit, and a day with 24 visits. When retrieving the data
-    // hourly, the day should be split up in 24 hour intervals with one visit
-    // each. So all in all the hour should now have 2 visits.
-    $this->assertEquals($data[$time]['paths']['/test']['visits'], 2);
+it('handles missing data', function () {
+  $time = new DateTimeImmutable('today midnight');
+  $from = $time->modify('-3 day');
+  $to = $time->modify('+2 day');
 
-    $stats->remove();
-  }
+  $stats = new KirbyStats(Interval::DAY);
+  $stats->handle('/test', $time->modify('-2 day'));
+  $stats->handle('/test', $time);
 
-  public function testSmallerInterval() {
-    $now = new DateTimeImmutable();
-    $from = $now->modify('yesterday');
-    $to = $now->modify('tomorrow');
+  $data = $stats->data(Interval::DAY, $from, $to);
 
-    $stats = new KirbyStats(Interval::WEEK);
-    $stats->handle('/test', $now);
+  // The data starts tree days ago and because at this time
+  // data recording hasn't started yet we expect the first day to be missing.
+  expect($data[$from->getTimestamp()]['missing'])->toBeTrue();
 
-    $stats = new KirbyStats(Interval::DAY);
-    $stats->handle('/test', $now);
+  // We expect one visit today and one visit two days ago. The day in between
+  // should be empty.
+  $timeStamp = $time->getTimestamp();
+  expect($data[$timeStamp]['paths']['/test']['counters']['visits'])->toBe(1);
 
-    $data = $stats->data($from, $to, Interval::WEEK);
-    $time = Interval::startOf(Interval::WEEK, $now)->getTimestamp();
-    // A week and a day with 1 visit each. When retrieving the data weekly, the
-    // should be added to the week.
-    $this->assertEquals($data[$time]['paths']['/test']['visits'], 2);
+  $timeStamp = $time->modify('-2 days')->getTimestamp();
+  expect($data[$timeStamp]['paths']['/test']['counters']['visits'])->toBe(1);
 
-    $stats->remove();
-  }
+  // Data ends tomorrow and because the date is in the future we expect it also
+  // to be missing.
+  expect($data[$time->modify('+1 day')->getTimestamp()]['missing'])->toBeTrue();
 
-  public function testMissingData() {
-    $now = new DateTimeImmutable();
-    $from = $now->modify('today');
-    $to = $now->modify('tomorrow');
-
-    $stats = new KirbyStats();
-    $data = $stats->data($from, $to, Interval::HOUR);
-    $time = Interval::startOf(Interval::HOUR, $now)->getTimestamp();
-    // Expect 24 missing entries when retrieving data for one day.
-    $this->assertEquals($data[$time]['missing'], true);
-    $this->assertEquals(count($data), 24);
-
-    $stats->remove();
-  }
-}
+  $stats->remove();
+});
