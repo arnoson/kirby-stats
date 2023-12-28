29b0f990dae40acbf54a289ed260dd25b71422a2
arnoson <hallo@arno-schlipf.info>
2023-12-28 15:16:31 +0100

    refactor: replace deprecated kirby components

--- src/views/k-stats-main-view.vue
+++ src/views/k-stats-main-view.vue
@@ -3,48 +3,44 @@
     <k-view class="k-stats-main-view">
       <k-header>
         Stats {{ page ? ` for ${labels.page}` : '' }}
-        <template #left>
+        <template #buttons>
           <k-button-group>
             <k-button icon="cog">Settings</k-button>
           </k-button-group>
-        </template>
-        <template #right>
           <k-button-group>
-            <k-button-link
+            <k-button
               :link="urls.last"
               icon="angle-left"
               :disabled="!urls.last"
             />
-            <k-dropdown>
-              <k-button @click="toggleIntervalSelect" icon="calendar">
-                {{ labels.date }}
+            <k-button @click="toggleIntervalSelect" icon="calendar">
+              {{ labels.date }}
+            </k-button>
+            <k-dropdown-content ref="intervalSelect">
+              <k-button class="k-dropdown-item" :link="urls.today">
+                Today
               </k-button>
-              <k-dropdown-content ref="intervalSelect">
-                <k-button-link class="k-dropdown-item" :link="urls.today">
-                  Today
-                </k-button-link>
-                <k-button-link class="k-dropdown-item" :link="urls['7-days']">
-                  Last 7 days
-                </k-button-link>
-                <k-button-link class="k-dropdown-item" :link="urls['30-days']">
-                  Last 30 days
-                </k-button-link>
-                <hr />
-                <k-button-link class="k-dropdown-item" :link="urls.day">
-                  Day
-                </k-button-link>
-                <k-button-link class="k-dropdown-item" :link="urls.week">
-                  Week
-                </k-button-link>
-                <k-button-link class="k-dropdown-item" :link="urls.month">
-                  Month
-                </k-button-link>
-                <k-button-link class="k-dropdown-item" :link="urls.year">
-                  Year
-                </k-button-link>
-              </k-dropdown-content>
-            </k-dropdown>
-            <k-button-link
+              <k-button class="k-dropdown-item" :link="urls['7-days']">
+                Last 7 days
+              </k-button>
+              <k-button class="k-dropdown-item" :link="urls['30-days']">
+                Last 30 days
+              </k-button>
+              <hr />
+              <k-button class="k-dropdown-item" :link="urls.day">
+                Day
+              </k-button>
+              <k-button class="k-dropdown-item" :link="urls.week">
+                Week
+              </k-button>
+              <k-button class="k-dropdown-item" :link="urls.month">
+                Month
+              </k-button>
+              <k-button class="k-dropdown-item" :link="urls.year">
+                Year
+              </k-button>
+            </k-dropdown-content>
+            <k-button
               icon="angle-right"
               :link="urls.next"
               :disabled="!urls.next"
@@ -76,27 +72,29 @@
         <k-stats-chart :stats="stats" :type="type" :page="labels.page" />
       </section>
 
-      <k-grid gutter="medium">
-        <k-column width="1/2">
-          <section class="k-section" style="padding-bottom: 1.5rem">
-            <header class="k-section-header">
-              <k-headline>Devices</k-headline>
-            </header>
-            <k-stats-browsers :stats="stats" />
-          </section>
-          <section class="k-section">
-            <k-stats-os :stats="stats" />
-          </section>
-        </k-column>
-        <k-column width="1/2">
-          <section class="k-section">
-            <header class="k-section-header">
-              <k-headline>Pages</k-headline>
-            </header>
-            <k-stats-pages :stats="stats" :urls="urls" :type="type" />
-          </section>
-        </k-column>
-      </k-grid>
+      <section class="k-section">
+        <k-grid style="gap: 1.5rem">
+          <k-column width="1/2">
+            <section class="k-section" style="padding-bottom: 1.5rem">
+              <header class="k-section-header">
+                <k-headline>Devices</k-headline>
+              </header>
+              <k-stats-browsers :stats="stats" />
+            </section>
+            <section class="k-section" style="margin-top: 0">
+              <k-stats-os :stats="stats" />
+            </section>
+          </k-column>
+          <k-column width="1/2">
+            <section class="k-section">
+              <header class="k-section-header">
+                <k-headline>Pages</k-headline>
+              </header>
+              <k-stats-pages :stats="stats" :urls="urls" :type="type" />
+            </section>
+          </k-column>
+        </k-grid>
+      </section>
     </k-view>
   </k-inside>
 </template>
@@ -165,6 +163,7 @@ export default {
   &:not(.active):not(:hover) {
     color: var(--color-gray-600);
   }
+
   &.active {
     font-weight: var(--font-bold);
     letter-spacing: 0;
@@ -176,6 +175,7 @@ export default {
 .k-stats-type-display {
   position: absolute;
 }
+
 .k-stats-type-width {
   visibility: hidden;
   letter-spacing: 0;
