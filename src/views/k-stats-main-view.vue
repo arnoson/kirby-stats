3ed4dcf002030ca6e4fbb97c1692e470f64ff22d
Jakob Grommas <jakobgrommas@jakob-grommas.localdomain>
2024-01-12 12:05:15 +0100

    fix: match Kirby 4 syntax for panel components
    BREAKING CHANGE: Removes extra padding of k-view. Might break on Kirby 3.

--- src/views/k-stats-main-view.vue
+++ src/views/k-stats-main-view.vue
@@ -1,99 +1,95 @@
 <template>
-  <k-inside>
-    <k-view class="k-stats-main-view">
-      <k-header>
-        Stats {{ page ? ` for ${labels.page}` : '' }}
-        <template #buttons>
-          <k-button-group>
-            <k-button
-              :link="urls.last"
-              icon="angle-left"
-              :disabled="!urls.last"
-            />
-            <k-button @click="toggleIntervalSelect" icon="calendar">
-              {{ labels.date }}
+  <k-panel-inside class="k-stats-main-view">
+    <k-header>
+      Stats {{ page ? ` for ${labels.page}` : '' }}
+      <template #buttons>
+        <k-button-group>
+          <k-button
+            :link="urls.last"
+            icon="angle-left"
+            :disabled="!urls.last"
+          />
+          <k-button @click="toggleIntervalSelect" icon="calendar">
+            {{ labels.date }}
+          </k-button>
+          <k-dropdown-content ref="intervalSelect">
+            <k-button class="k-dropdown-item" :link="urls.today">
+              Today
             </k-button>
-            <k-dropdown-content ref="intervalSelect">
-              <k-button class="k-dropdown-item" :link="urls.today">
-                Today
-              </k-button>
-              <k-button class="k-dropdown-item" :link="urls['7-days']">
-                Last 7 days
-              </k-button>
-              <k-button class="k-dropdown-item" :link="urls['30-days']">
-                Last 30 days
-              </k-button>
-              <hr />
-              <k-button class="k-dropdown-item" :link="urls.day">
-                Day
-              </k-button>
-              <k-button class="k-dropdown-item" :link="urls.week">
-                Week
-              </k-button>
-              <k-button class="k-dropdown-item" :link="urls.month">
-                Month
-              </k-button>
-              <k-button class="k-dropdown-item" :link="urls.year">
-                Year
-              </k-button>
-            </k-dropdown-content>
-            <k-button
-              icon="angle-right"
-              :link="urls.next"
-              :disabled="!urls.next"
-            />
-          </k-button-group>
-        </template>
-      </k-header>
-      <section class="k-section">
-        <header class="k-section-header">
-          <k-headline>
-            <button
-              class="k-stats-type-button"
-              :class="{ active: type === 'views' }"
-              @click="type = 'views'"
-            >
-              <span class="k-stats-type-display">Views</span>
-              <span class="k-stats-type-width">Views</span>
-            </button>
-            /
-            <button
-              class="k-stats-type-button"
-              :class="{ active: type === 'visits' }"
-              @click="type = 'visits'"
-            >
-              Visits
-            </button>
-          </k-headline>
-        </header>
-        <k-stats-chart :stats="stats" :type="type" :page="labels.page" />
-      </section>
+            <k-button class="k-dropdown-item" :link="urls['7-days']">
+              Last 7 days
+            </k-button>
+            <k-button class="k-dropdown-item" :link="urls['30-days']">
+              Last 30 days
+            </k-button>
+            <hr />
+            <k-button class="k-dropdown-item" :link="urls.day"> Day </k-button>
+            <k-button class="k-dropdown-item" :link="urls.week">
+              Week
+            </k-button>
+            <k-button class="k-dropdown-item" :link="urls.month">
+              Month
+            </k-button>
+            <k-button class="k-dropdown-item" :link="urls.year">
+              Year
+            </k-button>
+          </k-dropdown-content>
+          <k-button
+            icon="angle-right"
+            :link="urls.next"
+            :disabled="!urls.next"
+          />
+        </k-button-group>
+      </template>
+    </k-header>
+    <section class="k-section">
+      <header class="k-section-header">
+        <k-headline>
+          <button
+            class="k-stats-type-button"
+            :class="{ active: type === 'views' }"
+            @click="type = 'views'"
+          >
+            <span class="k-stats-type-display">Views</span>
+            <span class="k-stats-type-width">Views</span>
+          </button>
+          /
+          <button
+            class="k-stats-type-button"
+            :class="{ active: type === 'visits' }"
+            @click="type = 'visits'"
+          >
+            Visits
+          </button>
+        </k-headline>
+      </header>
+      <k-stats-chart :stats="stats" :type="type" :page="labels.page" />
+    </section>
 
-      <section class="k-section">
-        <k-grid style="gap: 1.5rem">
-          <k-column width="1/2">
-            <section class="k-section" style="padding-bottom: 1.5rem">
-              <header class="k-section-header">
-                <k-headline>Devices</k-headline>
-              </header>
-              <k-stats-browsers :stats="stats" />
-            </section>
-            <section class="k-section" style="margin-top: 0">
-              <k-stats-os :stats="stats" />
-            </section>
-          </k-column>
-          <k-column width="1/2">
-            <section class="k-section">
-              <header class="k-section-header">
-                <k-headline>Pages</k-headline>
-              </header>
-              <k-stats-pages :stats="stats" :urls="urls" :type="type" />
-            </section>
-          </k-column>
-        </k-grid>
-      </section>
-    </k-view>
-  </k-inside>
+    <section class="k-section">
+      <k-grid style="gap: 1.5rem">
+        <k-column width="1/2">
+          <section class="k-section" style="padding-bottom: 1.5rem">
+            <header class="k-section-header">
+              <k-headline>Devices</k-headline>
+            </header>
+            <k-stats-browsers :stats="stats" />
+          </section>
+          <section class="k-section" style="margin-top: 0">
+            <k-stats-os :stats="stats" />
+          </section>
+        </k-column>
+        <k-column width="1/2">
+          <section class="k-section">
+            <header class="k-section-header">
+              <k-headline>Pages</k-headline>
+            </header>
+            <k-stats-pages :stats="stats" :urls="urls" :type="type" />
+          </section>
+        </k-column>
+      </k-grid>
+    </section>
+  </k-panel-inside>
 </template>
 
 <script lang="ts">
