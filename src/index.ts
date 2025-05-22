9b8778a610f4085545e120d77e91c1beeaa8de08
arnoson <hallo@arno-schlipf.info>
2025-05-22 23:08:34 +0200

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-/// <reference types="vite/client" />
-
 import mainView from './views/main-view.vue'
 import percentCell from './components/percent-cell.vue'
 
 declare global {
   interface Window {
-    panel: any
-    __VUE_DEVTOOLS_GLOBAL_HOOK__: { Vue: Vue }
+    panel: {
+      plugin(name: string, options: any): void
+    }
   }
 }
 
@@ -14,12 +13,5 @@ window.panel.plugin('arnoson/kirby-stats', {
   components: {
     'k-table-kirby-stats-percent-cell': percentCell,
     'kirby-stats-main-view': mainView,
-  },
-  use: {
-    plugin(Vue: Vue) {
-      if (import.meta.env.MODE === 'development') {
-        window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = Vue
-      }
-    },
   },
 })
