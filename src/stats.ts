0cb0a3ae00c7a2d485aaa3b920d68872f6331fbb
arnoson <hallo@arno-schlipf.info>
2025-07-07 14:45:56 +0200

    chore: build stats

---
+++ src/stats.ts
@@ -0,0 +1,28 @@
+const events = ['click', 'scroll', 'keydown', 'mousemove', 'touchstart']
+const eventOptions: AddEventListenerOptions = { once: true, passive: true }
+let statsAreSend = false
+
+const addEventListeners = () =>
+  events.forEach((e) => document.addEventListener(e, sendStats, eventOptions))
+
+const removeEventListeners = () =>
+  events.forEach((e) =>
+    document.removeEventListener(e, sendStats, eventOptions),
+  )
+
+const sendStats = () => {
+  if (statsAreSend) return
+
+  const path = location.pathname.replace(/^\//, '')
+  fetch(`/kirby-stats/page/${path}`, { keepalive: true })
+  fetch(`/kirby-stats/site`, { keepalive: true })
+
+  removeEventListeners()
+  statsAreSend = true
+}
+
+const isReload = window.performance
+  .getEntriesByType('navigation')
+  .some((entry) => (entry as PerformanceNavigationTiming).type === 'reload')
+
+if (!isReload) addEventListeners()
